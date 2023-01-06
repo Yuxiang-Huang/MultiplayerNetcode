6ab3dd27d9a30f8c8f89df1d76e7438c85bf0bca
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-05 23:09:06 -0500

    Adding an UI

---
+++ Multiplayer/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void startHost()
+    {
+        NetworkingManager
+    }
+
+    void startClient()
+    {
+
+    }
+}
