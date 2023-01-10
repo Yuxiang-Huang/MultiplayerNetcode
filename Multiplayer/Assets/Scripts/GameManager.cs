47dc470e476a4261ef354cd119a1cb85c0e05324
Yuxiang Huang <yhuang30@stuy.edu>
2023-01-09 23:43:04 -0500

    UI for testing number of player sync done

---
+++ Multiplayer/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI display;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void generateNumber()
+    {
+        display.text = "" + Random.Range(0, 10);
+    }
+}
--- Multiplayer/Assets/Scripts/ScreenManager.cs
+++ Multiplayer/Assets/Scripts/ScreenManager.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
 public class ScreenManager : MonoBehaviour
 {
     public GameObject connect;
     public GameObject main;
+    public TextMeshProUGUI playersInGame;
+
+    private NetworkVariable<int> numofPlayer = new NetworkVariable<int>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        connect.SetActive(true);
+        main.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playersInGame.text = "Number of player: " + numofPlayer.Value;
     }
 
     public void startHost()
